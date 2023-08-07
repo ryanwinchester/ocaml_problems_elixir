a16b453d876cb479b2519c07dae771d45ae3571b
Ryan Winchester <ryan@sevenshores.co>
2023-08-07 14:23:34 -0300

    A few

--- lib/problems.ex
+++ lib/problems.ex
@@ -1,6 +1,72 @@
 defmodule Problems do
+  @moduledoc """
+  OCaml problems.
+  See: https://ocaml.org/problems
+  """
+
   @doc """
-  Remove duplicates from a list.
+  Returns the last element of a list.
+
+  ## Examples
+
+      iex> last([1, 2, 3])
+      3
+
+      iex> last([])
+      nil
+
+  """
+  def last([]), do: nil
+  def last([last]), do: last
+  def last([_ | tl]), do: last(tl)
+
+  @doc """
+  Returns the last two element of a list.
+
+  ## Examples
+
+      iex> last_two([1, 2, 3])
+      {2, 3}
+
+      iex> last_two([1])
+      nil
+
+      iex> last_two([])
+      nil
+
+  """
+  def last_two([last_1, last_2]), do: {last_1, last_2}
+  def last_two([_ | tl]), do: last_two(tl)
+  def last_two(list) when is_list(list), do: nil
+
+  @doc """
+  Find the N'th element of a list.
+
+  ## Examples
+
+      iex> nth([1, 2, 3], 0)
+      1
+
+      iex> nth([1, 2, 3], 2)
+      3
+
+      iex> nth([1, 2, 3], 3)
+      nil
+
+      iex> nth([], 0)
+      nil
+
+  """
+  def nth(list, at) do
+    nth(list, at, 0)
+  end
+
+  defp nth([], _, _), do: nil
+  defp nth([hd | _], at, at), do: hd
+  defp nth([_ | tl], at, i), do: nth(tl, at, i + 1)
+
+  @doc """
+  Eliminate consecutive duplicates of list elements.
 
   ## Examples
 
@@ -25,19 +91,19 @@ defmodule Problems do
 
   ## Examples
 
-      iex> pack_compress([1, 2, 2, 3, 4, 4, 4, 5])
+      iex> pack([1, 2, 2, 3, 4, 4, 4, 5])
       [[1], [2, 2], [3], [4, 4, 4], [5]]
 
-      iex> pack_compress(~w[a b b c d d d e])
+      iex> pack(~w[a b b c d d d e])
       [["a"], ["b", "b"], ["c"], ["d", "d", "d"], ["e"]]
 
   """
-  @spec pack_compress([any]) :: [any]
-  def pack_compress([hd | tl]) do
-    pack_compress(tl, [hd], [])
+  @spec pack([any]) :: [any]
+  def pack([hd | tl]) do
+    pack(tl, [hd], [])
   end
 
-  defp pack_compress([], sub, acc), do: Enum.reverse([sub | acc])
-  defp pack_compress([hd | tl], [hd | _] = sub, acc), do: pack_compress(tl, [hd | sub], acc)
-  defp pack_compress([hd | tl], sub, acc), do: pack_compress(tl, [hd], [sub | acc])
+  defp pack([], sub, acc), do: Enum.reverse([sub | acc])
+  defp pack([hd | tl], [hd | _] = sub, acc), do: pack(tl, [hd | sub], acc)
+  defp pack([hd | tl], sub, acc), do: pack(tl, [hd], [sub | acc])
 end
