0fb5976a52b66e766adbba59cfde2426b11e883e
Ryan Winchester <ryan@sevenshores.co>
2023-08-07 20:31:27 -0300

    Add some more

--- lib/problems.ex
+++ lib/problems.ex
@@ -162,4 +162,159 @@ defmodule Problems do
   defp pack([], sub, acc), do: reverse([sub | acc])
   defp pack([hd | tl], [hd | _] = sub, acc), do: pack(tl, [hd | sub], acc)
   defp pack([hd | tl], sub, acc), do: pack(tl, [hd], [sub | acc])
+
+  @doc """
+  Run-length encoding of a list.
+
+  ## Examples
+
+      iex> encode(~w[a a a a b c c a a d e e e e])
+      [{4, "a"}, {1, "b"}, {2, "c"}, {2, "a"}, {1, "d"}, {4, "e"}]
+
+  """
+  def encode(list) do
+    list
+    |> pack()
+    |> Enum.map(&{len(&1), hd(&1)})
+  end
+
+  @doc """
+  Modified run-length encoding.
+  If an element has no duplicates it is simply copied into the result list.
+  Only elements with duplicates are transferred as (N E) lists.
+
+  ## Examples
+
+      iex> modified_encode(~w[a a a a b c c a a d e e e e])
+      [{4, "a"}, "b", {2, "c"}, {2, "a"}, "d", {4, "e"}]
+
+  """
+  def modified_encode(list) do
+    list
+    |> pack()
+    |> Enum.map(fn
+      [char] -> char
+      chars -> {len(chars), hd(chars)}
+    end)
+  end
+
+  @doc """
+  Decode a run-length encoded list.
+
+  ## Examples
+
+      iex> decode([{4, "a"}, {1, "b"}, {2, "c"}, {2, "a"}, {1, "d"}, {4, "e"}])
+      ~w[a a a a b c c a a d e e e e]
+
+      iex> decode([{4, "a"}, "b", {2, "c"}, {2, "a"}, "d", {4, "e"}])
+      ~w[a a a a b c c a a d e e e e]
+
+  """
+  def decode(list) do
+    Enum.map(list, fn
+      {count, char} when is_integer(count) -> List.duplicate(char, count)
+      char -> char
+    end)
+    |> flatten()
+  end
+
+  @doc """
+  Don't explicitly create the sublists containing the duplicates, as in problem
+  "Pack consecutive duplicates of list elements into sublists", but only count
+  them
+
+  ## Examples
+
+      iex> modified_encode_direct(~w[a a a a b c c a a d e e e e])
+      [{4, "a"}, "b", {2, "c"}, {2, "a"}, "d", {4, "e"}]
+
+  """
+  def modified_encode_direct([current | tl]) do
+    modified_encode_direct(tl, current, [])
+  end
+
+  defp modified_encode_direct([], current, acc),
+    do: reverse([current | acc])
+
+  defp modified_encode_direct([hd | tl], hd, acc),
+    do: modified_encode_direct(tl, {2, hd}, acc)
+
+  defp modified_encode_direct([hd | tl], {count, hd}, acc),
+    do: modified_encode_direct(tl, {count + 1, hd}, acc)
+
+  defp modified_encode_direct([hd | tl], current, acc),
+    do: modified_encode_direct(tl, hd, [current | acc])
+
+  @doc """
+  Duplicate the elements of a list.
+
+  ## Examples
+
+      iex> duplicate(~w[a b c c d])
+      ~w[a a b b c c c c d d]
+
+  """
+  def duplicate(list) do
+    duplicate(list, [])
+  end
+
+  defp duplicate([], acc), do: reverse(acc)
+  defp duplicate([hd | tl], acc), do: duplicate(tl, [hd, hd | acc])
+
+  @doc """
+  Replicate the elements of a list a given number of times.
+
+  ## Examples
+
+      iex> replicate(~w[a b c], 3)
+      ~w[a a a b b b c c c]
+
+  """
+  def replicate(list, n) do
+    replicate(list, n, 1, [])
+  end
+
+  defp replicate([], _, _, acc), do: reverse(acc)
+  defp replicate([hd | tl], n, n, acc), do: replicate(tl, n, 1, [hd | acc])
+  defp replicate([hd | _] = list, n, i, acc), do: replicate(list, n, i + 1, [hd | acc])
+
+  @doc """
+  Drop every N'th element from a list.
+
+  ## Examples
+
+      iex> drop(~w[a b c d e f g h i k], 3)
+      ~w[a b d e g h k]
+
+  """
+  def drop(list, n) do
+    drop(list, n, 1, [])
+  end
+
+  defp drop([], _, _, acc), do: reverse(acc)
+  defp drop([_ | tl], n, n, acc), do: drop(tl, n, 1, acc)
+  defp drop([hd | tl], n, i, acc), do: drop(tl, n, i + 1, [hd | acc])
+
+  @doc """
+  Split a list into two parts; the length of the first part is given.
+
+  If the length of the first part is longer than the entire list, then the
+  first part is the list and the second part is empty.
+
+  ## Examples
+
+      iex> split(~w[a b c d e f g h i j], 3)
+      {~w[a b c], ~w[d e f g h i j]}
+
+      iex> split(~w[a b c d], 5)
+      {~w[a b c d], []}
+
+  """
+  def split(list, n) do
+    split(list, n, 0, [])
+  end
+
+  defp split([], _, _, acc), do: {reverse(acc), []}
+  defp split(list, n, n, acc), do: {reverse(acc), list}
+  defp split([hd | tl], n, i, acc), do: split(tl, n, i + 1, [hd | acc])
 end
